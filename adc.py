34ce3d337a8a590fa8291e0404d20b3a48ca6e85
Kenya Kornilov <sa5mli.xd@gmail.com>
2024-12-15 23:29:43 +0500

    silent mode + refactoring

--- adc.py
+++ adc.py
@@ -8,14 +8,15 @@ class Analyzer:
     ADC_ATTN = None
     left_level = 0
     right_level = 0
+    silent = True
     
     def __init__(self, PIN_LEFT, PIN_RIGHT, lp=0, ATTN=machine.ADC.ATTN_11DB, WDTH=machine.ADC.WIDTH_12BIT):
-        # Инициализация атрибутов с использованием self
-        self.ADC_LEFT = machine.ADC(machine.Pin(PIN_LEFT))
-        self.ADC_RIGHT = machine.ADC(machine.Pin(PIN_RIGHT))
+        self.ADC_LEFT = machine.ADC(machine.Pin(PIN_LEFT, mode=machine.Pin.IN, pull=machine.Pin.PULL_DOWN)) # похоже что pull_up pull_down не работают
+        self.ADC_RIGHT = machine.ADC(machine.Pin(PIN_RIGHT, mode=machine.Pin.IN, pull=machine.Pin.PULL_DOWN)) # похоже что pull_up pull_down не работают
         self.attn(ATTN)
         self.width(WDTH)
         self.LOW_PASS = lp  # Устанавливаем LOW_PASS
+        self.SILENT_COUNTER = 0
         
     def attn(self, ATTN):
         self.ADC_LEFT.atten(ATTN)
@@ -25,7 +26,7 @@ class Analyzer:
         self.ADC_LEFT.width(WDTH)
         self.ADC_RIGHT.width(WDTH)
         
-    def loop(self, ticks=150):
+    def loop(self, ticks=80):
         while True:
             max_left_level = 0
             max_right_level = 0
@@ -39,7 +40,6 @@ class Analyzer:
                 if max_right_level < loop_right_level: 
                     max_right_level = loop_right_level
 
-            time.sleep(0.0002)  # Небольшая пауза для стабилизации
             
             # temp for 11db attenuation
             max_left_level = self.__map(max_left_level, self.LOW_PASS, 2720, 0, 4095)
@@ -47,6 +47,17 @@ class Analyzer:
 
             self.left_level = max_left_level
             self.right_level = max_right_level
+
+            if self.left_level < 20 and self.right_level < 20:
+                if self.SILENT_COUNTER < 20:
+                    self.SILENT_COUNTER += 1
+                else:
+                    self.silent = True
+            else:
+                if self.SILENT_COUNTER > 1:
+                    self.SILENT_COUNTER = self.SILENT_COUNTER -1
+                else:
+                    self.silent = False
         
     def __map(self, x, in_min, in_max, out_min, out_max):
         return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
--- pixel.py
+++ pixel.py
@@ -2,29 +2,43 @@ import machine
 import neopixel
 import time
 
+from adc import Analyzer
+
 class UV:
     NEO_PIXEL = None
     LEDS_COUNT = None
     LEDS_OFFSET = 0
-    LEDS_INLINE = 0
+    LEDS_INLINE = -1
     
     # Настройка пропорций для цветов (0-1)
     RED_RATIO = 0.5     # Пропорция красного
     GREEN_RATIO = 0.0   # Пропорция зеленого
     BLUE_RATIO = 1.0    # Пропорция синего
-
     
-    def __init__(self, PIN_LED, LEDS_COUNT, LEDS_OFFSET=0):
+    def __init__(self, PIN_LED, LEDS_COUNT, LEDS_OFFSET=-1):
         self.NEO_PIXEL = neopixel.NeoPixel(machine.Pin(PIN_LED), LEDS_COUNT)
         self.LEDS_COUNT = LEDS_COUNT
         self.LEDS_OFFSET = LEDS_OFFSET
         
         self.LEDS_INLINE = (LEDS_COUNT - LEDS_OFFSET) // 2
+        self.SILENT = False
 
-    def loop(self, analyzer):
+    def loop(self, analyzer: Analyzer):
         while True:
             left_brightness = min(int(analyzer.left_level / (4095 / self.LEDS_INLINE)), self.LEDS_INLINE)
             right_brightness = min(int(analyzer.right_level / (4095 / self.LEDS_INLINE)), self.LEDS_INLINE)
+               
+            if analyzer.silent:
+                if not self.SILENT:
+                    self.SILENT = True
+                    for i in range(self.LEDS_COUNT):
+                        self.NEO_PIXEL[i] = self.__colorize(int(255 * 1))
+                    self.NEO_PIXEL.write()
+                
+                time.sleep(0.0001)
+                continue
+            else:
+                self.SILENT = False   
 
             # Очищаем все светодиоды
             for i in range(self.LEDS_COUNT): self.NEO_PIXEL[i] = (0, 0, 0)
@@ -43,7 +57,7 @@ class UV:
                     self.NEO_PIXEL[self.LEDS_INLINE + i + self.LEDS_OFFSET] = self.__colorize(color_intensity)
 
             self.NEO_PIXEL.write()  # Обновление LED-ленты
-            time.sleep(0.0001)  # Пауза для стабильности
+            #time.sleep(0.0001)  # Пауза для стабильности
     
     def __colorize(self, color_intensity):
         red_intensity = int(color_intensity * self.RED_RATIO)
